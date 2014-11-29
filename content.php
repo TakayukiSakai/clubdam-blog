e00e4105f188d610771987d88764e8b265a48545
clubdam <clubdam@dam.dam.l3.internal.cloudapp.net>
2014-11-29 16:03:30 +0000

    記事一覧画面で、記事詳細へのリンク部分を広範囲に

--- content.php
+++ content.php
@@ -10,7 +10,7 @@
  */
 ?>
 
-    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?> <?php if ( is_single()): ?>single<?php else: ?>not-single<?php endif;?>>
+    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?> <?php if ( is_single()): ?>single<?php else: ?>not-single onclick="location.href='<?php the_permalink(); ?>'"<?php endif;?>>
 		<?php if ( is_sticky() && is_home() && ! is_paged() ) : ?>
 		<div class="featured-post">
 			<?php _e( 'Featured post', 'twentytwelve' ); ?>
@@ -21,13 +21,7 @@
 				the_post_thumbnail();
 			endif; ?>
 
-			<?php if ( is_single() ) : ?>
 			<h1 class="entry-title"><?php the_title(); ?></h1>
-			<?php else : ?>
-			<h1 class="entry-title">
-				<a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
-			</h1>
-			<?php endif; // is_single() ?>
 			<?php if ( comments_open() ) : ?>
 				<div class="comments-link">
 					<?php comments_popup_link( '<span class="leave-reply">' . __( 'Leave a reply', 'twentytwelve' ) . '</span>', __( '1 Reply', 'twentytwelve' ), __( '% Replies', 'twentytwelve' ) ); ?>
